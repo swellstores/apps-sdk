dc98c57062397eb30e5dff4d5863a295ac379bda
Ignat Prokopovich <ignat@swell.is>
2025-02-14 13:14:03 +0000

    fix: replace problematic svg with corrected ones

--- src/liquid/filters/shopify/placeholder-svgs/index.test.ts
+++ src/liquid/filters/shopify/placeholder-svgs/index.test.ts
@@ -3,24 +3,40 @@ import svgs from './index';
 type Pair = [string, string];
 
 describe('placeholder-svgs', () => {
-  const [regular, apparel] = Object.entries(svgs).reduce<[Pair[], Pair[]]>(
-    (acc, pair) => {
-      // Svg images containing "apparel" in the name
-      // must have the "preserveAspectRatio" property to scale correctly.
-      // If the current svg files do not have this property,
-      // you can find it in the git history of the corresponding files.
-      const index = /\bapparel\b/.test(pair[0]) ? 1 : 0;
-      acc[index].push(pair);
-      return acc;
-    },
-    [[], []],
-  );
+  function getSvgTag(content: string): string {
+    return content.slice(0, content.indexOf('>') + 1);
+  }
 
-  it.each(regular)('svg %s', (name, content) => {
-    expect(content).not.toMatch('preserveAspectRatio');
+  describe('preserveAspectRatio', () => {
+    const [regular, apparel] = Object.entries(svgs).reduce<[Pair[], Pair[]]>(
+      (acc, pair) => {
+        // Svg images containing "apparel" in the name
+        // must have the "preserveAspectRatio" property to scale correctly.
+        // If the current svg files do not have this property,
+        // you can find it in the git history of the corresponding files.
+        const index = /\bapparel\b/.test(pair[0]) ? 1 : 0;
+        acc[index].push(pair);
+        return acc;
+      },
+      [[], []],
+    );
+
+    it.each(regular)('%s', (name, content) => {
+      expect(getSvgTag(content)).not.toMatch('preserveAspectRatio=');
+    });
+
+    it.each(apparel)('%s', (name, content) => {
+      expect(getSvgTag(content)).toMatch('preserveAspectRatio=');
+    });
   });
 
-  it.each(apparel)('svg %s', (name, content) => {
-    expect(content).toMatch('preserveAspectRatio');
+  describe('scale', () => {
+    it.each(Object.entries(svgs))('%s', (name, content) => {
+      // SVG tag should not contain fixed width and height,
+      // as this excludes free scaling.
+      // If the <svg /> tag contains these properties, you should remove them.
+      expect(getSvgTag(content)).not.toMatch('width=');
+      expect(getSvgTag(content)).not.toMatch('height=');
+    });
   });
 });
