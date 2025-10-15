11e753b0c553bf59e7b76786e5369ba0d0761f9a
SergeySwell <sergey@swell.is>
2025-10-15 16:25:59 +0200

    use deferWith

--- src/compatibility/shopify-objects/predictive_search.ts
+++ src/compatibility/shopify-objects/predictive_search.ts
@@ -22,7 +22,7 @@ export default function ShopifyPredictiveSearch(
   }
 
   return new ShopifyResource<ShopifyPredictiveSearch>({
-    performed: defer(() => search.performed),
+    performed: deferWith(search, (search) => search.performed),
     resources: ShopifyPredictiveSearchResources(instance, search),
     terms: defer(() => search.query),
     types: ['product'],
