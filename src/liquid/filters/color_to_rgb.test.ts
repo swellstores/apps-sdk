6e36cd3ad48177b08fb296a19991306aaa77c01a
Vladislav Studenichnik <vladislav@swell.is>
2025-12-02 18:51:41 +0100

    fix tests

--- src/liquid/filters/color_to_rgb.test.ts
+++ src/liquid/filters/color_to_rgb.test.ts
@@ -4,12 +4,12 @@ describeFilter('color_to_rgb', (render) => {
   it('should convert color to rgb', async () => {
     const result = await render(`{{ '#EA5AB9' | color_to_rgb }}`);
 
-    expect(result).toBe('rgb(234, 90, 185)');
+    expect(result).toBe('234 90 185');
   });
 
   it('should convert color to rgb with alpha channel', async () => {
     const result = await render(`{{ '#EA5AB9' | color_to_rgb: 0.5 }}`);
 
-    expect(result).toBe('rgba(234, 90, 185, 0.5)');
+    expect(result).toBe('234 90 185 / 0.50');
   });
 });
